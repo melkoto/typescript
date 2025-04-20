04f8c19ae4e3d2782b6d52ace922c004f850854b
Movsar Makhmutov <m.mahmutov@urbi.ae>
2025-04-20 18:28:23 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import './style.css'
+import { main } from "./index.ts";
+import typescriptLogo from './typescript.svg'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>TypeScript</h1>
+  </div>
+`
+
+window.onload = () => {
+  main()
+}
